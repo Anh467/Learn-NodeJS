832826bb6ec668b59e5a3cb13244806ba0a7f957
Anh467 <nguyenanhviet080702@gmail.com>
2023-09-30 18:18:39 +0700

    add model Sequelize dbo.FolderCourse

---
+++ src/resources/app/models/foldercourse.js
@@ -0,0 +1,47 @@
+module.exports= (sequelize, Sequelize) => {
+    const foldercourse = sequelize.define('FolderCourse',{
+        FolderID:{
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        CustomerID:{
+            type: Sequelize.STRING(11),
+            allowNull: false,
+            references:{
+                model: "CUSTOMER",
+                key: "CustomerID"
+            }
+        },
+        FolderName: {
+            type: Sequelize.STRING(50),
+        },
+        FolderImg: {
+            type: Sequelize.STRING(200),
+        },
+        Description: {
+            type: Sequelize.STRING(500),
+        },
+        privacry: {
+            type: Sequelize.STRING(10),
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("GETDATE()"), 
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("GETDATE()"), 
+        },
+    }, {
+        tableName: 'FolderCourse', 
+        indexes: [
+          {
+            unique: true,
+            fields: ['CustomerID', 'FolderName'],
+            name: 'compositeKey', 
+          },
+        ],
+      });
+}
