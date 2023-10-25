8416947109c05dbd81b8840d81269ee96943ebbd
Anh467 <nguyenanhviet080702@gmail.com>
2023-10-25 09:47:43 +0700

    finish all promise for the api of quiz

--- src/public/js/api/quizAPI.js
+++ src/public/js/api/quizAPI.js
@@ -25,10 +25,10 @@ const API_DELETE_QUIZ = {
 }
 
 // [GET]    {/:quizzesid}        get list quiz according quizzesID
-function getQuiz(QuizzesID) {
+getQuiz = function(quizzesid) {
     return new Promise(function(resolve, reject) {
       $.ajax({
-        url: `${API_GET_QUIZ.url}/${QuizzesID}`,
+        url: `${API_GET_QUIZ.url}/${quizzesid}`,
         type: API_GET_QUIZ.method,
         contentType: "application/json",
         success: function(data) {
@@ -42,15 +42,53 @@ function getQuiz(QuizzesID) {
   }
 // [POST]   {/:quizzesid}        create new quiz according quizzesID
 createQuiz = function(quizzesid, inputBody){
-
+    return new Promise(function(resolve, reject) {
+        $.ajax({
+            url: `${API_CREATE_QUIZ.url}/${quizzesid}`,
+            type: API_CREATE_QUIZ.method,
+            contentType: "application/json",
+            data: JSON.stringify(inputBody),
+            success: function(data) {
+              resolve(data);
+            },
+            error: function(xhr) {
+              reject(xhr.responseText);
+            }
+        });
+    });
 }
 // [DELETE] {/:quizzesid/:index} delete question according quizzesID and index of question
 deleteQuiz = function(quizzesid, index){
-
+    return new Promise(function(resolve, reject) {
+        $.ajax({
+            url: `${API_DELETE_QUIZ.url}/${quizzesid}/${index}`,
+            type: API_DELETE_QUIZ.method,
+            contentType: "application/json",
+            success: function(data) {
+              resolve(data);
+            },
+            error: function(xhr) {
+              reject(xhr.responseText);
+            }
+        });
+    })
 }
 // [PUT] {/:quizzesid/:index} update quiz according quizzesID and index of question
-updateQuiz = function(quizzesid, index){
-
+updateQuiz = function(quizzesid, index, inputBody){
+    return new Promise(function(resolve, reject) {
+        $.ajax({
+            url: `${API_UPDATE_QUIZ.url}/${quizzesid}/${index}`,
+            type: API_UPDATE_QUIZ.method,
+            contentType: "application/json",
+            data: JSON.stringify(inputBody),
+            success: function(data) {
+              resolve(data);
+            },
+            error: function(xhr) {
+              reject(xhr.responseText);
+            }
+        });
+    })
 }
 // module.exports = {
 //     getQuiz: getQuiz,
